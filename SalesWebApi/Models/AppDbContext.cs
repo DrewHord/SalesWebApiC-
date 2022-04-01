07b56f3869270175babb7c19bd033ee113c44a4c
Andrew Hord <drewhord@gmail.com>
2022-04-01 13:41:37 -0400

    Initial Commit

---
+++ SalesWebApi/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebApi.Models;
+
+namespace SalesWebApi.Models {
+    public class AppDbContext : DbContext {
+
+        public virtual DbSet<OrderLine> OrderLines { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+
+        }
+
+        public DbSet<SalesWebApi.Models.Order> Order { get; set; }
+    }
+}
